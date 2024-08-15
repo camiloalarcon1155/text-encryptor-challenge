c6415aeb5c9e6aad79245fe9add573a26e83ffca
Camilo Alarcón <camiloalarcon11155@gmail.com>
2024-08-15 17:08:28 -0500

    Adicionado btn desencriptar y parte de su funcion para activar o desactivar salida de texto encriptado. aun falta proceso de encriptado

--- indexEncriptador.js
+++ indexEncriptador.js
@@ -2,6 +2,7 @@ console.log(` eres un gran tipo y mereces ser fullStack`);
 
 const textarea = document.getElementById('textoIngresado');
 const btnEncriptar = document.querySelector('.buttonEncriptar');
+const btnDesencriptar = document.querySelector('.buttonDesencriptar');
 const textoParaEncriptar = document.querySelector('.textoParaEncriptar');
 const pMostrarTexto = document.querySelector('.pMostrarTextoTratado');
 const sectionTextoEncriptadoNoEncontrado = document.querySelector('.sectionTextoEncriptado');
@@ -12,6 +13,7 @@ const btnCopiarTextoTratado = document.querySelector('.buttonCopiarTexto');
 // Evento de input para ajustar la altura cuando el usuario escribe
 textarea.addEventListener('input', autoResize);
 btnEncriptar.addEventListener('click', encriptarTexto);
+btnDesencriptar.addEventListener('click', desencriptarTexto);
 btnCopiarTextoTratado.addEventListener('click', copiarTextoTratado)
 
 
@@ -23,6 +25,21 @@ function autoResize() {
   
 
 function encriptarTexto() {
+    
+    if (textoParaEncriptar.value) {
+        console.log(`${textoParaEncriptar.value}`)
+        pMostrarTexto.innerText = textoParaEncriptar.value;
+        sectionTextoEncriptadoNoEncontrado.classList.add('inactive'); 
+        sectionTextoEncriptadoEncontrado.classList.remove('inactive');
+        console.log(`${textoParaEncriptar.value} se encuentra en el textarea`);
+    } else {
+        sectionTextoEncriptadoEncontrado.classList.add('inactive');
+        sectionTextoEncriptadoNoEncontrado.classList.remove('inactive');
+        console.log(`no hay nada en el textarea`);
+    } 
+}
+
+function desencriptarTexto() {
     
     if (textoParaEncriptar.value) {
         console.log(`${textoParaEncriptar.value}`)
@@ -52,5 +69,4 @@ function copiarTextoTratado() {
     textareaTemporal.setSelectionRange(0, 99999);
     navigator.clipboard.writeText(textareaTemporal.value);
     document.body.removeChild(textareaTemporal);
-    alert("Copied the text: " + textareaTemporal.value);
 }
